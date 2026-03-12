77a223abb99150e99d399be5e028f9c6f6eb6358
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-12 11:20:52 -0700

    add platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cmake -S . -B build -G Ninja -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release ${CMAKE_ARGS}
 cmake --build build
+if [[ ${build_platform} == ${target_platform} ]]; then
 ctest --test-dir build --output-on-failure
+fi
 cmake --install build
